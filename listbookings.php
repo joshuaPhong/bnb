650b8dd8575ec67ffe46d4440eb3404a96ad7c8e
joshuaPhong <klx2044@yahoo.com>
2022-12-05 22:19:09 +1300

    list bookings by booking ID

--- listbookings.php
+++ listbookings.php
@@ -13,7 +13,7 @@ echo '<div id="content">';
 include "config.php"; //load in any variables
 $DBC = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBDATABASE);
 
-//insert DB code from here onwards
+
 //check if the connection was good
 if (mysqli_connect_errno()) {
     echo "Error: Unable to connect to MySQL. ".mysqli_connect_error() ;
@@ -21,37 +21,37 @@ if (mysqli_connect_errno()) {
 }
 
 //prepare a query and send it to the server
-$query = 'SELECT roomID,roomname,roomtype FROM room ORDER BY roomtype';
+$query = 'SELECT bookingID,customerID FROM booking ORDER BY bookingID';
 $result = mysqli_query($DBC,$query);
 $rowcount = mysqli_num_rows($result); 
 ?>
 <h1>Current Bookings</h1>
-<h2><a href='makeabooking.php'>[Add a room]</a><a href="index.php">[Return to main page]</a></h2>
+<h2><a href='makeabooking.php'>[Make A Booking]</a><a href="index.php">[Return to main page]</a></h2>
 <table border="1">
     <thead>
         <tr>
-            <th>Room Name</th>
-            <th>Type</th>
+            <th>Booking ID</th>
+            <th>Customer ID</th>
             <th>Action</th>
         </tr>
     </thead>
     <?php
 
-//makes sure we have rooms
+//makes sure we have bookings
 if ($rowcount > 0) {  
     while ($row = mysqli_fetch_assoc($result)) {
-	  $id = $row['roomID'];	
-	  echo '<tr><td>'.$row['roomname'].'</td><td>'.$row['roomtype'].'</td>';
-	  echo     '<td><a href="viewroom.php?id='.$id.'">[view]</a>';
+	  $id = $row['bookingID'];	
+	  echo '<tr><td>'.$row['bookingID'].'</td><td>'.$row['customerID'].'</td>';
+	  echo     '<td><a href="viewbookings.php?id='.$id.'">[view]</a>';
 	  
       //check if we have permission to modify data
       if (isAdmin()) {
-        echo         '<a href="editroom.php?id='.$id.'">[edit]</a>';
-        echo         '<a href="deleteroom.php?id='.$id.'">[delete]</a></td>';
+        echo         '<a href="editbookings.php?id='.$id.'">[edit]</a>';
+        echo         '<a href="deletebookings.php?id='.$id.'">[delete]</a></td>';
       }
       echo '</tr>'.PHP_EOL;
    }
-} else echo "<h2>No rooms found!</h2>"; //suitable feedback
+} else echo "<h2>No bookings found!</h2>"; //suitable feedback
 echo "</table>";
 mysqli_free_result($result); //free any memory used by the query
 mysqli_close($DBC); //close the connection once done
