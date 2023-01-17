223bec2dd087345c26ed591a8bed753e5d143751
joshuaPhong <klx2044@yahoo.com>
2023-01-17 21:40:07 +1300

    removed if isset from action.php

--- action.php
+++ action.php
@@ -7,13 +7,11 @@ if (mysqli_connect_errno()) {
 }
 $todate = $_POST['to_date'];
 $fromdate = $_POST['from_date'];
+$orderData = "";
+$query = "select * from room where roomID not in (select roomID from booking where checkindate >= '$fromdate' and checkoutdate <= '$todate')";
+$result = mysqli_query($db_connection, $query);
 
-if (isset($_POST["from_date"], $_POST["to_date"])) {
-    $orderData = "";
-    $query = "select * from room where roomID not in (select roomID from booking where checkindate >= '$fromdate' and checkoutdate <= '$todate')";
-    $result = mysqli_query($db_connection, $query);
-
-    $orderData .= '
+$orderData .= '
     <table class="table table-bordered">
     <tr>
     <th width="5%">Room ID</th>
@@ -23,9 +21,9 @@ if (isset($_POST["from_date"], $_POST["to_date"])) {
     
     </tr>';
 
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            $orderData .= '
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        $orderData .= '
             <tr>
             <td>' . $row["roomID"] . '</td>
             <td>' . $row["roomname"] . '</td>
@@ -33,13 +31,12 @@ if (isset($_POST["from_date"], $_POST["to_date"])) {
             <td>' . $row["beds"] . '</td>
     
             </tr>';
-        }
-    } else {
-        $orderData .= '
+    }
+} else {
+    $orderData .= '
         <tr>
         <td colspan="5">No rooms Found</td>
         </tr>';
-    }
-    $orderData .= '</table>';
-    echo $orderData;
 }
+$orderData .= '</table>';
+echo $orderData;
